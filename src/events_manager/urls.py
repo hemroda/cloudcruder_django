6b8823a49e4080cf9451f8b5cc5b3a1036b9c49a
hemroda <hemroda@gmail.com>
2022-10-24 19:42:06 +0200

    forms for new tasks & events

--- src/events_manager/urls.py
+++ src/events_manager/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.events, name="events_path"),
     path("<int:id>", views.event_detail, name="event_detail_path"),
+    path("new", views.new, name="new_event_path")
 ]
--- src/tasks_manager/urls.py
+++ src/tasks_manager/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.tasks, name="tasks_path"),
+    path("new", views.new, name="new_task_path")
 ]
