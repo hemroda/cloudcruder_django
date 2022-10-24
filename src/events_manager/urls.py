c38946b6f853bac7e9ff50918aad79212f6558ae
hemroda <hemroda@gmail.com>
2022-10-24 14:21:00 +0200

    events_manager: move urls declaration in app and include them in base app

---
+++ src/events_manager/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.events, name="events_path"),
+    path("<int:id>", views.event_detail, name="event_detail_path"),
+]
--- src/events_manager/views.py
+++ src/events_manager/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from events_manager.models import Event
 
 
 def events(request):
-    return render(request, "events_manager/events.html")
+    return render(request, "events_manager/events.html", {"events": Event.objects.all(), "num_events": Event.objects.count})
 
 
-def event(request):
-    return render(request, "events_manager/event.html")
+def event_detail(request, id):
+    event = get_object_or_404(Event, pk=id)
+    return render(request, "events_manager/event.html", {"event": event})
