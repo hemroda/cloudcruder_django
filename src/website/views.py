ae68b63e0659fbf6ab186930898d1b4b473871da
hemroda <hemroda@gmail.com>
2022-10-24 15:31:55 +0200

    remove templates unecessary duplications

--- src/website/views.py
+++ src/website/views.py
@@ -1,8 +1,13 @@
 from datetime import datetime
 from django.shortcuts import render
 
+
 def homepage(request):
-    return render(request, "website/homepage.html", context={"name": "John", "footer_date": datetime.today()})
+    return render(request, "website/homepage.html",
+                  context={"page_title": "Maavita", "name": "John", "footer_date": datetime.today()})
+
 
 def about(request):
-    return render(request, "website/about.html", context={"footer_date": datetime.today()})
+    return render(request, "website/about.html", context={
+        "page_title": "About - Maavita", "footer_date": datetime.today()
+    })
