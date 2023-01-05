933aeb0a082bdf44f205ad3d8ca95e32615455de
hemroda <hemroda@gmail.home>
2023-01-05 11:09:30 +0100

    add dashboard section for logged in users

--- backend/static/js/index.js
+++ backend/static/js/index.js
@@ -1,1 +1,16 @@
 console.log("JS loaded")
+
+/*
+  Mobile nav
+*/
+const mobileMenuButton = document.querySelector("#mobile-menu-button")
+const mobileMenu = document.querySelector("#mobile-menu")
+const closeMobileMenu = document.querySelector("#close-mobile-menu")
+const openMobileMenu = document.querySelector("#open-mobile-menu")
+
+mobileMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden")
+  closeMobileMenu.classList.toggle("hidden")
+  openMobileMenu.classList.toggle("block")
+  openMobileMenu.classList.toggle("hidden")
+})
