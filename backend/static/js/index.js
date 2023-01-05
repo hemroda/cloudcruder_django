be255605fbb209d115cc9268bdabf3474c01a9b5
hemroda <hemroda@gmail.home>
2023-01-05 13:22:37 +0100

    update login fields ui

--- backend/static/js/index.js
+++ backend/static/js/index.js
@@ -8,9 +8,11 @@ const mobileMenu = document.querySelector("#mobile-menu")
 const closeMobileMenu = document.querySelector("#close-mobile-menu")
 const openMobileMenu = document.querySelector("#open-mobile-menu")
 
-mobileMenuButton.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden")
-  closeMobileMenu.classList.toggle("hidden")
-  openMobileMenu.classList.toggle("block")
-  openMobileMenu.classList.toggle("hidden")
-})
+if (mobileMenuButton) {
+  mobileMenuButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden")
+    closeMobileMenu.classList.toggle("hidden")
+    openMobileMenu.classList.toggle("block")
+    openMobileMenu.classList.toggle("hidden")
+  })
+}
